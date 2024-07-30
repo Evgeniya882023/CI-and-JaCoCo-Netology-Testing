b77a526b1ee003e581e7d08b941ba49b7709afef
Evgeniya Malashenko <malashenko_genya@mail.ru>
2024-07-30 23:10:41 +0300

    Second commit

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -8,9 +8,21 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
     }
+}
